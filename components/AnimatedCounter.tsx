a6838489b3356770305388c76e159e343f68710d
Samrat Jha <samratj@amazon.com>
2024-05-05 23:57:09 -0400

    feat: Add balance box with animated counter

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className='w-full'>
+      <CountUp
+        decimals={2}
+        end={amount} decimal='.' prefix='$'/>
+    </div>
+  )
+}
+
+export default AnimatedCounter
