5198184aef2a1d10a949e9fa3a2a17e3a1bac170
Samrat Jha <samratj@amazon.com>
2024-06-15 23:38:08 -0400

    feat: Recent transactions setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
@@ -6,6 +7,7 @@ import { getLoggedInUser } from '@/lib/actions/users.actions';
 import React from 'react'
 
 const Home = async ( { searchParams: { id, page }}: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id })
 
@@ -30,6 +32,8 @@ const Home = async ( { searchParams: { id, page }}: SearchParamProps) => {
                     totalBanks={accounts?.totalBanks}
                     totalCurrentBalance={accounts?.totalCurrentBalance}
                   />
+
+                  <RecentTransactions accounts={accountsData} transactions={account?.transactions} appwriteItemId={appwriteItemId} page={currentPage}/>
             </header>
         </div>
         <RightSidebar user={loggedIn} transactions={accounts?.transactions} banks={accountsData?.slice(0, 2)} />
