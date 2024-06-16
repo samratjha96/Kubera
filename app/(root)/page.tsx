adb25476e7ff1daf192e64f57c6a86b5e705d5fe
Samrat Jha <samratj@amazon.com>
2024-06-15 23:13:49 -0400

    feat: Integrate bank account fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/users.actions';
 import React from 'react'
 
-const Home = async () => {
+const Home = async ( { searchParams: { id, page }}: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id })
+
+  if(!accounts) return;
+
+  const accountsData = accounts?.data
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId
+  const account = await getAccount({ appwriteItemId })
   return (
     <section className="home">
         <div className='home-content'>
@@ -18,19 +26,13 @@ const Home = async () => {
                   />
 
                   <TotalBalanceBox 
-                    accounts={[]}
-                    totalBanks={1}
-                    totalCurrentBalance={1200.35}
+                    accounts={accountsData}
+                    totalBanks={accounts?.totalBanks}
+                    totalCurrentBalance={accounts?.totalCurrentBalance}
                   />
             </header>
         </div>
-        <RightSidebar user={loggedIn} transactions={[]} banks={[
-          {
-            currentBalance: 123.50
-          }, {
-            currentBalance: 145.50
-          }
-        ]} />
+        <RightSidebar user={loggedIn} transactions={accounts?.transactions} banks={accountsData?.slice(0, 2)} />
     </section>
   )
 }
