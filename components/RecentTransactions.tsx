4198da732acc27d593f10dc37f442b00f38c5971
Samrat Jha <samratj@amazon.com>
2024-06-15 23:49:13 -0400

    feat: Render transactions in table

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
+import TransactionsTable from './TransactionsTable'
 
 const RecentTransactions = ({
   accounts,
@@ -31,6 +32,7 @@ const RecentTransactions = ({
         {accounts.map((account: Account) => (
           <TabsContent value={account.appwriteItemId} key={account.id} className='space-y-4'>
             <BankInfo account={account} appwriteItemId={appwriteItemId} type='full'/>
+            <TransactionsTable transactions={transactions} />
           </TabsContent>
         ))}
       </Tabs>
